8ad2c854ec747af4818198383df5d8e4a5379ccb
Melvin Lu <melvinlu1996@gmail.com>
2025-09-27 00:41:19 -0400

    Initial Commit

---
+++ snapzify/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  snapzify
+//
+//  Created by Melvin Lu on 9/27/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
---
+++ snapzifyTests/snapzifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  snapzifyTests.swift
+//  snapzifyTests
+//
+//  Created by Melvin Lu on 9/27/25.
+//
+
+import Testing
+@testable import snapzify
+
+struct snapzifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
